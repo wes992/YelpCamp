abf7e70aa49a5d7556fd83441acd2c10033bd297
wes992 <wespipkin@gmail.com>
2019-11-26 20:01:11 +0000

    first commit

---
+++ app.js
@@ -0,0 +1,82 @@
+var express      = require('express'),
+	app          = express(),
+	bodyParser   = require('body-parser'),
+	mongoose     = require('mongoose'),
+	Campground   = require('./models/campground'),
+	Comment      = require('./models/comment'),
+	seedDB       = require('./seeds');
+
+mongoose.set("useUnifiedTopology", true);
+mongoose.connect("mongodb://localhost/yelp_camp", {useNewUrlParser: true});
+app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({extended: true}));
+seedDB();
+
+
+
+app.get("/", function(req, res){
+	res.render("landing");
+});
+
+
+// INDEX - show all campgrounds
+app.get("/campgrounds", function(req,res){
+	// Get all campgrounds from DB
+	Campground.find({}, function(err, allCampgrounds){
+		if(err){
+			console.log(err)
+		} else{
+			res.render("index", {campgrounds:allCampgrounds});
+		}
+	});
+});
+
+
+// CREATE - add new campground to DB
+app.post("/campgrounds", function(req,res){
+	
+	// get data from form and add to camapgrounds array
+	var name  = req.body.name,
+		image = req.body.image,
+		desc  = req.body.description,
+		newCampground = {name:name, image:image, description: desc};
+	
+	//create a new campground and save to DB
+	Campground.create(newCampground, function(err, newlyCreated){
+		if(err){
+			console.log(err);
+		} else {
+			//redirect back to campgrounds page
+			res.redirect('/campgrounds');
+		}
+	})
+
+});
+
+
+// NEW - show form to create new campgrounds
+app.get("/campgrounds/new", function(req, res){
+	res.render("new.ejs");
+});
+
+
+// SHOW- shows more info about one campground
+app.get("/campgrounds/:id", function(req, res){
+	
+	// find campgeound with provided ID
+	Campground.findById(req.params.id).populate('comments').exec(function(err, foundCampground){
+		if(err){
+			console.log(err);
+		} else {
+			//render show temple with that campground
+			console.log(foundCampground);
+			res.render('show', {campground: foundCampground});	
+			
+		}
+	});	
+
+})
+app.listen(process.env.PORT || 3000 , process.env.IP, function(){
+		   console.log('YelpCamp Server Started!');
+});
+
